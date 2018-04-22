f9de851c461bd19789b93ded0397adcb59bfc622
Manuel Dell'Elce <mh025r0x@gmail.com>
2018-04-22 18:18:12 +0200

    Fix link.sh

--- link.sh
+++ link.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 CWD=$PWD
 for filename in ../webmiddle/packages/*; do
-    for ((i=0; i<=3; i++)); do
-        cd $filename
-        yarn link
-        cd $CWD
-        yarn link $(basename $filename)
-    done
+  cd $filename
+  yarn link
+  cd $CWD
+  yarn link $(basename $filename)
 done
