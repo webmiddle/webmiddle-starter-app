00899acb13867a8635a841b699d638a076a882ea
Manuel Dell'Elce <mh025r0x@gmail.com>
2018-11-04 12:09:27 +0100

    server: read apiKey from API_KEY env variable

--- src/server.js
+++ src/server.js
@@ -22,7 +22,8 @@ const server = new Server(
     ...extractTestServices(tests)
   },
   {
-    port: process.env.PORT || 3000
+    port: process.env.PORT || 3000,
+    apiKey: process.env.API_KEY || ""
   }
 );
 server.start();
