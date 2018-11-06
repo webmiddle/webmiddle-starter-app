74c3d678a24ac5b6b7691dab3e5b9e52de91fd8c
Manuel Dell'Elce <mh025r0x@gmail.com>
2018-11-06 19:58:47 +0100

    Remove obsolete commented out code

--- src/index.js
+++ src/index.js
@@ -1,19 +1,16 @@
-// TODO: use the framework to define a search project with Main.js as entrypoint
+import { rootContext } from "webmiddle";
+import path from "path";
+import fs from "fs";
+import FetchPageLinks from "./services/FetchPageLinks";
 
-//import { SearchProject } from 'webmiddle-project-search';
-import { rootContext } from 'webmiddle';
-import path from 'path';
-import fs from 'fs';
-import FetchPageLinks from './services/FetchPageLinks';
-
-rootContext.evaluate(
-  <FetchPageLinks
-    name="hackernews"
-    url="https://news.ycombinator.com/"
-    query="show hn"
-  />
-).then(resource => {
-  console.log(
-    JSON.stringify(resource, null, 2)
-  );
-});
+rootContext
+  .evaluate(
+    <FetchPageLinks
+      name="hackernews"
+      url="https://news.ycombinator.com/"
+      query="show hn"
+    />
+  )
+  .then(resource => {
+    console.log(JSON.stringify(resource, null, 2));
+  });
